7af4c1146c610538a176941f5972ff3f0dc50907
masakiAndMarron <84778070+masakiAndMarron@users.noreply.github.com>
2025-02-17 23:34:37 +0900

    メンテナンスページ追加

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  async redirects() {
+    return process.env.MAINTENANCE_MODE === "true"
+      ? [
+          {
+            source: "/((?!maintenance.html$).*$)",
+            destination: "/maintenance.html",
+            permanent: false,
+          },
+        ]
+      : [];
+  },
 };
+
+module.exports = {};
 
 export default nextConfig;
