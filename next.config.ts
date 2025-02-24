ef99bfb4109d58bb58bb0ffd61b4cddd80f4f26e
masakiAndMarron <84778070+masakiAndMarron@users.noreply.github.com>
2025-02-24 22:38:50 +0900

    maintenance

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   async redirects() {
-    return process.env.MAINTENANCE_MODE === "true"
-      ? [
-          {
-            source: "/((?!maintenance.html$).*$)",
-            destination: "/maintenance.html",
-            permanent: false,
-          },
-        ]
-      : [];
+    return [
+      {
+        source: "/((?!maintenance.html$).*$)",
+        destination: "/maintenance.html",
+        permanent: false,
+      },
+    ];
   },
 };
 
